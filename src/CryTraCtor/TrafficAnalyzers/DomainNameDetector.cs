c8509b6fc03ceec42c71280779eb7d6e498b429d
DenisHromada <xhroma16@stud.fit.vutbr.cz>
2024-03-27 03:09:33 +0100

    feat: output dns query and answer domains

--- src/CryTraCtor/TrafficAnalyzers/DomainNameDetector.cs
+++ src/CryTraCtor/TrafficAnalyzers/DomainNameDetector.cs
@@ -1,16 +1,19 @@
-﻿using SharpPcap;
+﻿using CryTraCtor.DataStructures;
+using Kaitai;
+using SharpPcap;
 using SharpPcap.LibPcap;
 
 namespace CryTraCtor.TrafficAnalyzers;
 
 public class DomainNameDetector(string analyzedFileName) : TrafficAnalyzer(analyzedFileName)
 {
+    protected static int DnsPacketCounter = 0;
     public override void Run()
     {
         using var device = new CaptureFileReaderDevice(AnalyzedFileName);
 
         device.Open();
-        device.Filter = "ip and tcp";
+        device.Filter = "ip and udp and port 53";
 
         device.OnPacketArrival += HandlePacketArrival;
         
@@ -23,14 +26,54 @@ public class DomainNameDetector(string analyzedFileName) : TrafficAnalyzer(analy
         var rawPacket = packetCapture.GetPacket();
         var packet = PacketDotNet.Packet.ParsePacket(rawPacket.LinkLayerType, rawPacket.Data);
         
-        var tcpPacket = packet.Extract<PacketDotNet.TcpPacket>();
-        if (tcpPacket == null) {return;}
+        var udpPacket = packet.Extract<PacketDotNet.UdpPacket>();
+        if (udpPacket == null) {return;}
+
+        var ipPacket = (PacketDotNet.IPPacket)udpPacket.ParentPacket;
         
-        var ipPacket = (PacketDotNet.IPPacket)tcpPacket.ParentPacket;
+        var dataStream = new KaitaiStream(udpPacket.PayloadData);
+        var data = new DnsPacket(dataStream);
+        DnsPacketCounter++;
+        Console.WriteLine("DNS packet #{0}", DnsPacketCounter);
+        Console.WriteLine("Transaction ID: {0:X}", data.TransactionId);
+        
+        foreach (var query in data.Queries)
+        {
+            var domainName = string.Empty;
+            foreach (var subDomain in query.Name.Name)
+            {
+                domainName += subDomain.Name + ".";
+            }
+            Console.WriteLine("Query: {0}", domainName.TrimEnd('.'));
+        }
+        
+        foreach (var answer in data.Answers)
+        {
+            var domainName = string.Empty;
+            foreach (var subDomain in answer.Name.Name)
+            {
+                if (subDomain.IsPointer)
+                {
+                    var actualName = subDomain.Pointer.Contents;
+                    foreach (var actualSubDomain in actualName.Name)
+                    {
+                        domainName += actualSubDomain.Name + ".";
+                    }
+                    
+                }
+                else
+                {
+                    domainName += subDomain.Name + ".";
+                }
+            }
+            Console.WriteLine("Answer: {0}", domainName.TrimEnd('.'));
+        }
+        
+        // var protocol = udpPayloadPacket.
         var srcIp = ipPacket.SourceAddress;
         var dstIp = ipPacket.DestinationAddress;
-        int srcPort = tcpPacket.SourcePort;
-        int dstPort = tcpPacket.DestinationPort;
+        int srcPort = udpPacket.SourcePort;
+        int dstPort = udpPacket.DestinationPort;
 
         Console.WriteLine("{0}:{1} -> {2}:{3}",
             srcIp, srcPort, dstIp, dstPort);
