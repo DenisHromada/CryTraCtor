3799651f1b1ecdf19f6fa8f137be002df469bc30
DenisHromada <xhroma16@stud.fit.vutbr.cz>
2025-03-06 12:22:37 +0100

    fix: migration script

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -6,7 +6,4 @@
 # Load environment variables
 . ./.env
 
-POSTGRES_HOST=localhost #name resolution is not available outside the Docker network
-DB_CONNECTION_STRING="Server=$POSTGRES_HOST;Port=$POSTGRES_PORT;Database=$POSTGRES_DB;Username=$POSTGRES_USER;Password=$POSTGRES_PASSWORD"
-
-dotnet ef database update --connection "$DB_CONNECTION_STRING" --project ./src/CryTraCtor.Database
+dotnet ef database update --connection "${DATABASE_CONNECTION_STRING}" --project ./src/CryTraCtor.Database
